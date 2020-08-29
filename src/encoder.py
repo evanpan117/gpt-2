65e60dd6f9e23c601b7584c4e1a7604e78d4456a
Robin Rowe <robin.rowe@venturehollywood.com>
2020-08-28 22:26:21 -0700

    Fixed deprecation warnings and a utf-8 code page error

--- src/encoder.py
+++ src/encoder.py
@@ -96,7 +96,7 @@ class Encoder:
     def encode(self, text):
         bpe_tokens = []
         for token in re.findall(self.pat, text):
-            token = ''.join(self.byte_encoder[b] for b in token.encode('utf-8'))
+            token = ''.join(self.byte_encoder[b] for b in token.encode('utf-8', errors=self.errors))
             bpe_tokens.extend(self.encoder[bpe_token] for bpe_token in self.bpe(token).split(' '))
         return bpe_tokens
 
